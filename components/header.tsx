00eba2b840d2b14b20b6208f807c5c0b3012201f
Amin Jamali <aminjamali79@gmail.com>
2026-02-21 13:27:42 +0330

    Added theme change button

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import ThemeButton from './theme-toggle-btn'
 
 export const SiteHeader = () => {
   return (
@@ -10,12 +11,13 @@ export const SiteHeader = () => {
           <Image src="/BTP_Logo.png" alt="Logo of the company" width={122} height={67} />
         </Link>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center space-x-4">
         <nav className="space-x-4">
           <Link href="/">Home</Link>
           <Link href="/about">about</Link>
           <Link href="/contact-us">contact us</Link>
         </nav>
+        <ThemeButton />
       </div>
     </header>
 

23eb43856b3c9babebe2d52beb7da9da0730c482
Amin Jamali <aminjamali79@gmail.com>
2026-02-22 11:47:10 +0330

    Removed unused import

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { cn } from '@/lib/utils'; // shadcn cn helper (optional but recommended)
 
 export default function SiteFooter() {
   return (

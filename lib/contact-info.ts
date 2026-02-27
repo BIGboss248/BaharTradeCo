1bda131e66df19f342c741b18e67238d356e6784
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-02-27 15:47:11 +0330

    Added social links

--- lib/contact-info.ts
+++ lib/contact-info.ts
@@ -3,3 +3,5 @@ export const Email = "contact@bahartradeco.com";
 export const EmailLink = "mailto:contact@bahartradeco.com"
 export const TelLink = "tel:+983132647777"
 export const Tel = "+98 313 264 7777"
+export const whatsAppLink = "https://wa.me/+983136284259"
+export const instagramLink = "https://www.instagram.com/bahartradeco"
